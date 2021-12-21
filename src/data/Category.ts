04095a655d584cbd92481aff3aa6d33db9cdefe9
yoshioster <yoshioster@gmail.com>
2021-12-21 17:30:31 +0900

    カテゴリ毎の入力内容を定義したcategorySchema を追加。

--- src/data/Category.ts
+++ src/data/Category.ts
@@ -1,3 +1,30 @@
+export interface CategorySchema {
+  name: string;
+  schema: {
+    id: string;
+    type: string;
+    property: string;
+  }[];
+}
+
+export const categorySchema: CategorySchema[] = [
+  {
+    name: 'ウィスキー',
+    schema: [
+      { id: 'title', type: 'text', property: 'title' },
+      { id: 'body', type: 'textarea', property: 'body' },
+    ]
+  },
+  {
+    name: '脱出ゲーム',
+    schema: [
+      { id: 'title', type: 'text', property: 'title' },
+      { id: 'shop', type: 'text', property: 'shop' },
+      { id: 'body', type: 'textarea', property: 'body' },
+    ]
+  },
+]
+
 export const categoryList: string[] = [
   'ウィスキー',
   '脱出ゲーム',
